f54417c015e160f2ac225e41db011cc8917059b6
Jon Richards <jrseti@gmail.com>
2023-04-22 10:39:52 -0700

    edit

--- trading_apis/config.py
+++
@@ -1,7 +0,0 @@
-# encoding: utf-8
-
-ALPACA_API_KEY = "PKP9VKDV18ZDQ0BK0E7L"
-ALPACA_API_SECRET_KEY = "hM5BifEgrxjyyzWbFpda253AWi4p3SUOhVl1zCDW"
-ALPACA_API_URL = "https://paper-api.alpaca.markets"
-
-IS_PAPER = True
